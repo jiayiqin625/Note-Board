53f189d56aedeaf742b21d9aa66f0effd7396085
Jiayi Qin <jiayiqin0625@gmail.com>
2026-08-20 12:49:36 -0700

    rate limited

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -3,7 +3,7 @@ import { Redis } from "@upstash/redis";
 
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(60, "60s"),
+  limiter: Ratelimit.slidingWindow(100, "60s"),
 });
 
 export default ratelimit;
